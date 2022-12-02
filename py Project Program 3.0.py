121bafe36a070f079f8156007dc9d02e10416ac9
brungg <119693753+brungg@users.noreply.github.com>
2022-12-02 13:49:14 -0500

    Update py Project Program 3.0.py
    hi

--- py Project Program 3.0.py
+++ py Project Program 3.0.py
@@ -1,23 +1,27 @@
-
 """
 Project Program
 Name: Andrew Barbarini, Eric Davis, Bradyn Weatherwax
 Date: 11/29/2022
 """
 
-import turtle, tkinter
-from turtle import Turtle, Screen
-from tkinter.colorchooser import askcolor
+import turtle
 from tkinter import *
+from turtle import Turtle, Screen
+from tkinter import ttk
+from tkinter.colorchooser import askcolor
 
 screen = Screen()
 canvas = screen.getcanvas()
 canvas.create_window(-200, -200)
 t = Turtle()
+t.speed(-1)
+t.pensize(10)
+b = "black"
+f = DISABLED
 
 def changeColor():
     global b
-    a,b = askcolor(title = "Choose Color")
+    a,b = askcolor(title="Choose Color")
     t.pencolor(b)
     t.pensize(10)
 
@@ -33,18 +37,24 @@ def rect():
 def erase():
     t.pencolor("white")
     t.pensize(50)
-    
-def undo():
-    t.undo()
 
 def sFill():
+    global f
+    f = NORMAL
     t.fillcolor(b)
     t.begin_fill()
 
 def eFill():
+    global f
+    f = DISABLED
     t.end_fill()
     t.fillcolor("black")
     t.pencolor(b)
+
+def undo():
+    for i in range(10):
+        t.undo()
+    t.pensize(10)
 
 def clear():
     t.clear()
@@ -60,27 +70,31 @@ def clickRight(x, y):
     t.goto(x, y)
     t.pd()
 
-def buttonMaker(text, cmd):
-    tkinter.Button(canvas.master, text = text, bg = "white", command = cmd).pack()
-
+def buttonMaker(text, cmd, dis):
+    return ttk.Button(canvas.master, text = text, state = dis, command = cmd)
+    
 def main():
     turtle.listen()
-
-    t.speed(-1)
-    t.pensize(10)
-    b = "black"
 
     t.ondrag(drawing)
     turtle.onscreenclick(clickRight)
 
-    buttonMaker("Pick Color", changeColor)
-    buttonMaker("Undo", undo)
-    buttonMaker("Clear", clear)
-    buttonMaker("Erase", erase)
-    buttonMaker("Triangle", triangle)
-    buttonMaker("Rectangle", rect)
-    buttonMaker("Start Fill", sFill)
-    buttonMaker("End Fill", eFill)
+    pc = buttonMaker("Pick Color", changeColor, NORMAL).pack()
+    u = buttonMaker("Undo", undo, NORMAL).pack()
+    c = buttonMaker("Clear", clear, NORMAL).pack()
+    e = buttonMaker("Erase", erase, NORMAL).pack()
+    ti = buttonMaker("Triangle", triangle, NORMAL).pack()
+    r = buttonMaker("Rectangle", rect, NORMAL).pack()
+    sf = buttonMaker("Start Fill", sFill, NORMAL).pack()
+    ef = buttonMaker("End Fill", eFill, DISABLED)
+    if f == NORMAL:
+        print("a")
+        ef['state'] = NORMAL
+        ef.pack()
+    else:
+        print("a")
+        ef['state'] = DISABLED
+        ef.pack()
 
     screen.mainloop()
 
